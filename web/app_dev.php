682dd68cb45b2ccce39c5ca42d6e7e06a07be03c
NuBOX DevCom <slye@nubox.fr>
2018-01-31 09:50:35 +0100

    Fix permissions to app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,10 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (PHP_SAPI === 'cli-server') {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
-}
 
 require __DIR__ . '/../vendor/autoload.php';
 Debug::enable();
